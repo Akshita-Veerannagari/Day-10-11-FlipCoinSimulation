d2fdb214a237e186844d2257ea3d3787bdd0c362
Akshita-Veerannagari <akshitareddy.v@gmail.com>
2020-07-13 22:36:56 +0530

    Looping a coin simulator [No. of times heads/tails won]

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,10 +1,17 @@
 #! /bin/bash
 
-num=$(( RANDOM ))
-if [ $(( num % 2)) -eq 0 ]
-then
-        echo "Head is winner"
-else
-        echo "Tail is winner"
-fi
+headCount=0
+tailCount=0
+for(( counter=1; counter<=10; counter++ ))
+do
+        num=$(( RANDOM ))
+        if [ $(( num % 2)) -eq 0 ]
+        then
+                (( headCount++ ))
+        else
+                (( tailCount++ ))
+        fi
+done
+echo "Heads has won $headCount times"
+echo "Tails has won $tailCount times"
 
